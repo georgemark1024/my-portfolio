56a61e12d17be15b21a8894f167bf02ca783fe39
George Mark <144449716+georgemark1024@users.noreply.github.com>
2026-02-06 12:06:20 +0300

    fix: Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://www.georgemark.dev/',
 	output: 'static',
 	integrations: [mdx(), sitemap()],
 });
